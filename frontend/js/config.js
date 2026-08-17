737ae202babe0391530918c92e4bd94ede7dc103
anurag0044 <anuragbanerjee103@gmail.com>
2026-08-18 00:55:28 +0530

    co

---
+++ frontend/js/config.js
@@ -0,0 +1,15 @@
+// Local environment configurations (ignored by git)
+export const ENV = {
+  FIREBASE_CONFIG: {
+    
+   apiKey: "AIzaSyDbOeC-PGHcGytwnr-q1JeadIaEYa00MsY",
+  authDomain: "larvae-lens-32908.firebaseapp.com",
+  projectId: "larvae-lens-32908",
+  storageBucket: "larvae-lens-32908.firebasestorage.app",
+  messagingSenderId: "207137173299",
+  appId: "1:207137173299:web:d704b121361cefa99f5846",
+  measurementId: "G-6TNCNXS9P4"
+  },
+  API_BASE_URL: "https://larvae-lens-backend.onrender.com",
+  RAZORPAY_KEY_ID: "rzp_test_09f1RW6egHRwCD"
+};
