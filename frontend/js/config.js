97e600442e99acb18263bc12d5bd3346b37dba9b
anurag0044 <anuragbanerjee103@gmail.com>
2026-08-18 17:10:16 +0530

    backend url changed

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -10,6 +10,6 @@ export const ENV = {
   appId: "1:207137173299:web:d704b121361cefa99f5846",
   measurementId: "G-6TNCNXS9P4"
   },
-  API_BASE_URL: "https://larvae-lens-backend.onrender.com",
+  API_BASE_URL: "https://larvae-lens-backend-908924004639.us-central1.run.app",
   RAZORPAY_KEY_ID: "rzp_test_09f1RW6egHRwCD"
 };
